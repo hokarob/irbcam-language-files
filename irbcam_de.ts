b2d15a84f5d57ced8d941f1ed8d4393a6c10128d
IRBCAM client CI/CD <technical@irbcam.com>
2025-10-02 15:28:49 +0000

    Added new strings for translation

--- irbcam_de.ts
+++ irbcam_de.ts
@@ -5959,25 +5959,25 @@ Dropdown menu text. %1 = currently active mode (E.g. Index Mode: No Group)</extr
 <context>
     <name>SettingsInterface</name>
     <message>
-        <location filename="../../../backend/src/settingsinterface.cpp" line="147"/>
+        <location filename="../../../backend/src/settingsinterface.cpp" line="149"/>
         <source>Failed to translate language to %1</source>
         <extracomment>Error message. %1 = language</extracomment>
         <translation>Fehler beim Übersetzen der Sprache in %1</translation>
     </message>
     <message>
-        <location filename="../../../backend/src/settingsinterface.cpp" line="156"/>
+        <location filename="../../../backend/src/settingsinterface.cpp" line="160"/>
         <source>Language changed</source>
         <extracomment>Status message</extracomment>
         <translation>Sprache geändert</translation>
     </message>
     <message>
-        <location filename="../../../backend/src/settingsinterface.cpp" line="327"/>
+        <location filename="../../../backend/src/settingsinterface.cpp" line="335"/>
         <source>Failed to save settings for %1</source>
         <extracomment>Error message: %1 = full name (of user)</extracomment>
         <translation>Fehler beim Speichern der Einstellungen für %1</translation>
     </message>
     <message>
-        <location filename="../../../backend/src/settingsinterface.cpp" line="333"/>
+        <location filename="../../../backend/src/settingsinterface.cpp" line="341"/>
         <source>Settings for %1 saved</source>
         <extracomment>Status message. %1 = full name (of user)</extracomment>
         <translation>Einstellungen für %1 gespeichert</translation>
